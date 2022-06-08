7f5918a9e8ff4165b4fed7b2468cf3deee1ea889
Nasir Barnes <nabarnes@fb.com>
2022-06-08 13:54:27 -0700

    Logout Button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -49,6 +51,10 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setAdapter(adapter);
         populateHomeTimeline();
     }
+    void onLogoutButton() {
+
+
+    }
 
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
@@ -72,5 +78,18 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure " + response , throwable);
             }
         });
+    }
+
+    public void onLogoutButton(View view) {
+        // forget who's logged in
+
+
+        TwitterApp.getRestClient(this).clearAccessToken();
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(i);
+        finish();
     }
 }
